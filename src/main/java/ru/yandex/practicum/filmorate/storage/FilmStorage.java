a4308ccf8b5d23af3769b3acf48e8153ac637c86
Artem Oleshev <sunlightpattern@yandex.ru>
2024-12-04 23:58:23 +0300

    fix code style

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface FilmStorage extends AbstractStorage<Long, Film> {
     List<Film> getPopularFilms(int count);
+
     List<Film> getAllByIds(List<Long> ids);
 }
